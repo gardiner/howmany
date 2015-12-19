2e3902f1a38b192f993e665e0b1d1b19be69cf32
Ole Trenner <ole@jayotee.de>
2015-12-19 23:44:33 +0100

    adjusted output format
    * reduced number of visible table lines
    * adjusted margins

--- js/howmany.config.js
+++ js/howmany.config.js
@@ -5,7 +5,7 @@ define(['jquery'], function($) {
         defaults = {
             //constants
             DATEFORMAT: 'ddd, DD.MM.',
-            ALWAYS_VISIBLE_ROWS: 15,
+            ALWAYS_VISIBLE_ROWS: 10,
             COLORS: ['#274060', '#335C81', '#65AFFF', '#1B2845', '#5899E2'],
             PIECHART_THRESHOLD: 0.015,
             TIMELINE_DAYS: 15,
